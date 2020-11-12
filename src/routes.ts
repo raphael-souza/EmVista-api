2b88b66e01c334ec76fedc55f8befd9aaf38c986
raphael-souza <raphael3130@gmail.com>
2020-11-11 22:42:15 -0300

    init crud fin. assets

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response} from 'express';
+import { getFinAsset, getFinAssets, saveFinAsset } from './controller/FinancialAssetController';
 import { finishedTask, getTask, getTasks, removedTask, savetask, updateTask } from './controller/TaskController';
 
 const routes = Router()
@@ -13,6 +14,10 @@ routes.post('/task', savetask);
 routes.put('/task/:id', updateTask);
 routes.patch('/task/:id', finishedTask);
 routes.delete('/task/:id', removedTask);
+
+routes.get('/financial-assets', getFinAssets);
+routes.get('/financial-asset/:id', getFinAsset);
+routes.post('/financial-asset', saveFinAsset);
 
 
 export default routes;
