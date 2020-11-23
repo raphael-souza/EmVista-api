b01548a28ceff01a5bc3a707bd7e4a59cc41de7d
raphael-souza <raphael3130@gmail.com>
2020-11-23 07:57:00 -0300

    implementando jwt

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,6 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Length, IsNotEmpty } from "class-validator";
+import * as bcrypt from "bcryptjs";
 
 @Entity()
 export class User {
@@ -7,6 +9,7 @@ export class User {
     id: number;
 
     @Column()
+    @Length(4, 20)
     firstName: string;
 
     @Column()
@@ -14,5 +17,29 @@ export class User {
 
     @Column()
     age: number;
+
+    @Column()
+    @Length(4, 100)
+    password: string;
+
+    @Column()
+    @IsNotEmpty()
+    role: string;
+
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8);
+    }
+
+    checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+        return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import "reflect-metadata";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as helmet from "helmet";
+// import * as cors from "cors";
 import routes from "./routes";
 import { createConnection } from 'typeorm';
 
 const cors = require('cors');
-const app = express();
-createConnection();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(routes);
+createConnection()
+  .then(async connection => {
+    // app instance
+    const app = express();
+    
+    // midlewares
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(helmet());
 
-app.listen(3333);
+    app.use(routes);
+
+    app.listen(3333, () => {
+      console.log("emVista started ! in port 3333");
+    });
+  })
+  .catch(error => console.log(error));
+
+
