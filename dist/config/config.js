51ee781ee02e0c5d4148ee7e6d383f6ff3039767
raphael-souza <raphael3130@gmail.com>
2021-03-28 17:40:23 -0300

    novo projeto corrigindo erros

--- dist/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: "@GetJwt&mVist@"
-};
-//# sourceMappingURL=config.js.map
--- src/database/conect.ts
+++
@@ -1,7 +0,0 @@
-import { createConnection } from 'typeorm';
-
-createConnection().then(async connection => {
-  console.log('Conected whith database');
-
-})
-.catch(error => console.log(error));
